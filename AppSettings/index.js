97376351605143f69d6262cbe9b1a306b6faafc7
ToniNichev <toni.nichev@gmail.com>
2019-04-02 19:11:19 -0400

    Work

--- AppSettings/index.js
+++ AppSettings/index.js
@@ -13,39 +13,42 @@ class AppSettings {
   loadSettings() {
     let filepath = this.filepath;
 
-    try {
-      if (fs.existsSync(filepath)) {
-        //settings exists
-        fs.readFile(filepath, 'utf-8', (err, data) => {
-          if(err){
-              console.log("Error loading settings!");
-              return;
-          }    
-    
-          this.config = JSON.parse(data); 
-          if(typeof this.callback != 'undefined') {
-            this.callback();
-          }
-        });        
-      }
-    } catch(err) {
+    if (fs.existsSync(filepath)) {
+      //settings exists
+      fs.readFile(filepath, 'utf-8', (err, data) => {
+        if(err){
+            console.log("Error loading settings!");
+            return;
+        }    
+        this.config = JSON.parse(data); 
+        if(typeof this.callback != 'undefined') {
+          this.callback();
+        }
+      });        
+    }
+    else {
+      console.log("No config file, let's create it for the first time");
       // create config for the first time
-      let appSettingsConfig = '{"firstTimeRun":1,"syncConfigs":[]}';
-      saveSettings(appSettingsConfig);
-    }    
+      this.config  = JSON.parse('{"firstTimeRun":1,"syncConfigs":[]}');
+      this.saveSettings(this.config, () => { this.loadSettings(); });      
+    }
   }
 
-  saveSettings(appSettingsConfig) {   
+  saveSettings(appSettingsConfig, callbackFunction) {   
     //this.config.syncConfigs = appSettingsConfig;
     this.config = appSettingsConfig;
     //this.config.firstTimeRun = 0;
     let filepath = this.filepath;
+    console.log("Saving file :", filepath);
     let cfg = JSON.stringify(this.config);
     fs.writeFile(filepath, cfg,  function (err) {
       console.log("SAVED");
       if(err){
           console.log("An error ocurred reading the file :" + err.message);
           return;
+      }else {
+        if(typeof callbackFunction != 'undefined')
+          callbackFunction();
       }    
     });     
   }
